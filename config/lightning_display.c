d7ad75cb1cb966037d76681537a30bda70ff7c70
ruansonder-r <ruan.l@agrigateone.com>
2025-07-25 15:58:15 +0200

    qwdas

--- config/lightning_display.c
+++ config/lightning_display.c
@@ -60,32 +60,6 @@ static void draw_lightning(struct zmk_display_buffer *buffer, uint8_t x, uint8_t
     }
 }
 
-// Custom render function that extends the default status screen
-static int lightning_status_render(struct zmk_display_buffer *buffer) {
-    uint32_t current_time = k_uptime_get();
-    
-    // Draw cloud in top-right corner
-    draw_cloud(buffer, DISPLAY_WIDTH - 25, 2);
-    
-    // Handle lightning animation
-    if (lightning_active) {
-        uint32_t elapsed = current_time - lightning_start_time;
-        
-        if (elapsed < 150) { // 150ms animation duration
-            lightning_frame = (elapsed * 8) / 150;
-            uint8_t lightning_length = 25 - (lightning_frame * 2);
-            
-            if (lightning_length > 0) {
-                draw_lightning(buffer, DISPLAY_WIDTH - 15, 10, lightning_length);
-            }
-        } else {
-            lightning_active = false;
-        }
-    }
-    
-    return 0;
-}
-
 // WPM event handler
 static int lightning_wpm_listener(const zmk_event_t *eh) {
     struct zmk_wpm_state_changed *ev = as_zmk_wpm_state_changed(eh);
@@ -111,9 +85,26 @@ static int lightning_wpm_listener(const zmk_event_t *eh) {
 ZMK_LISTENER(lightning_display, lightning_wpm_listener)
 ZMK_SUBSCRIPTION(lightning_display, zmk_wpm_state_changed);
 
-// Register custom render function
-static const struct zmk_status_screen lightning_status_screen = {
-    .render = lightning_status_render,
-};
-
-DEVICE_DT_INST_DEFINE(0, NULL, NULL, NULL, NULL, POST_KERNEL, CONFIG_APPLICATION_INIT_PRIORITY, &lightning_status_screen); 
+// Simple lightning widget that can be called from the main display
+void lightning_widget_render(struct zmk_display_buffer *buffer) {
+    uint32_t current_time = k_uptime_get();
+    
+    // Draw cloud in top-right corner
+    draw_cloud(buffer, DISPLAY_WIDTH - 25, 2);
+    
+    // Handle lightning animation
+    if (lightning_active) {
+        uint32_t elapsed = current_time - lightning_start_time;
+        
+        if (elapsed < 150) { // 150ms animation duration
+            lightning_frame = (elapsed * 8) / 150;
+            uint8_t lightning_length = 25 - (lightning_frame * 2);
+            
+            if (lightning_length > 0) {
+                draw_lightning(buffer, DISPLAY_WIDTH - 15, 10, lightning_length);
+            }
+        } else {
+            lightning_active = false;
+        }
+    }
+} 
